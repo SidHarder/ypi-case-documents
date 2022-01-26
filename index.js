ec6b9ff48b34616e3bc03e8988cef800eebb3c2d
sidharder <sideharder@gmail.com>
2022-01-26 07:56:31 -1000

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var dotenv = require('dotenv').config();
 var fs = require('fs');
 
 var express = require('express');
@@ -8,17 +9,14 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true })
 var expressHandlebars = require('express-handlebars');
 var engine = expressHandlebars.engine;
 
-var port = 50071;
 var path = require('path');
 
-app.use('/documents', express.static('/mnt/cfileserver/accessiondocuments'));
-app.use('/scanning', express.static('/mnt/cfileserver/documents/scanning'));
+app.use('/documents', express.static(process.env.ACCESSION_DOCUMENTS_LOCATION));
+app.use('/scanning', express.static(process.env.SCANNED_DOCUMENTS_LOCATION));
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-
-var rootFolder = `/mnt/cfileserver/accessiondocuments`;
 
 var isDirectory = directoryName => {
   return fs.lstatSync(directoryName).isDirectory()
@@ -28,75 +26,153 @@ var isFile = fileName => {
   return fs.lstatSync(fileName).isFile()
 }
 
-var directories = fs.readdirSync('/mnt/cfileserver/documents/scanning').map(directoryName => {
-  return path.join('/mnt/cfileserver/documents/scanning', directoryName)
-}).filter(isDirectory)
-
-var mappedDirectories = directories.map(function (dir) {
-  var displayName = dir.split('/')[5];
-  var url = `/scanfiles/${displayName}`;
-  return { filePath: dir, displayName: displayName, url: url };
-})
+var isRequisition = fileName => {
+  var regx = /.REQ/;
+  return regx.test(fileName);
+}
 
 app.get('/scanfolders', (req, res) => {
+  renderScanFolders(res);
+});
+
+function renderScanFolders (res) {
+  var directories = fs.readdirSync(process.env.SCANNED_DOCUMENTS_LOCATION).map(directoryName => {
+    return path.join(process.env.SCANNED_DOCUMENTS_LOCATION, directoryName)
+  }).filter(isDirectory);
+
+  var mappedDirectories = directories.map(function (dir) {
+    var dirName = path.basename(dir);
+    var url = `/scanfiles/${dirName}`;
+    return { filePath: dir, displayName: dirName, url: url };
+  });
+
   res.render('scanfolders', {
     directories: mappedDirectories
   });
-});
+}
 
 app.get('/scanfiles/:folderName', (req, res) => {
-  var folderName = req.params.folderName;  
+  var data = {
+    folderName: req.params.folderName,
+    folderUrl: `/scanfolders`
+  }
+  
+  renderScanFiles(data, res);
+});
 
-  var files = fs.readdirSync(`/mnt/cfileserver/documents/scanning/${folderName}`).map(fileName => {
-    return path.join(`/mnt/cfileserver/documents/scanning/${folderName}`, fileName)
-  }).filter(isFile)
+function renderScanFiles (data, res) {
+  var files = fs.readdirSync(path.join(process.env.SCANNED_DOCUMENTS_LOCATION, data.folderName)).map(fileName => {
+    return path.join(process.env.SCANNED_DOCUMENTS_LOCATION, data.folderName, fileName);
+  }).filter(isFile);
 
-  var mappedFiles = files.map(function (file) {
-    var displayName = file.split('/')[6];
-    var url = `/scanfiles/${folderName}/${displayName}`;
-    return { filePath: file, displayName: displayName, url: url };
+  data.files = files.map(function (file) {
+    var fileName = path.basename(file);
+    var url = `/scanfiles/${data.folderName}/${fileName}`;
+    return { filePath: file, displayName: fileName, url: url };
   })
 
-  res.render('scanfiles', {
-    folderName: folderName,
-    files: mappedFiles
-  });
-});
+  res.render('scanfiles', data);
+}
 
 app.get('/scanfiles/:folderName/:fileName', (req, res) => {
-  var folderName = req.params.folderName;
-  var fileName = req.params.fileName;
-  var url = `http://10.1.2.90:50071/scanning/${folderName}/${fileName}`;  
-  res.render('scanfile', {
-    url: url,
-    folderName: folderName,
-    fileName: fileName
-  });
-});
-
-app.post('/scanfiles/:folderName/:fileName/move', urlencodedParser , (req, res) => {    
-  var filePath = getCasePath(req.body.masterAccessionNo);
-
-  res.render('scanfile_moved', {
+  var data = {
+    folderName: req.params.folderName,
     fileName: req.params.fileName,
-    url: `http://10.1.2.90:50071/scanfiles/${req.params.folderName}`,
-    displayName: req.params.folderName,
-    filePath: filePath
-  });
+    url: `/scanning/${req.params.folderName}/${req.params.fileName}`
+  }  
+  res.render('scanfile', data);
 });
 
-function moveFile()
+app.post('/scanfiles/:folderName/:fileName/move', urlencodedParser , (req, res) => { 
+  var data = {
+    folderName: req.params.folderName,
+    fileName: req.params.fileName,
+    existingPath: path.join(process.env.SCANNED_DOCUMENTS_LOCATION, req.params.folderName, req.params.fileName),
+    masterAccessionNo: req.body.masterAccessionNo,
+    action: req.body.buttonSubmit
+  }
+
+  var newPath = getCasePath(data.masterAccessionNo);
+  var maRegx = /^\d\d-\d+/g;
+
+  if (maRegx.test(data.masterAccessionNo) == false) {
+    renderInvalidMasterAccessionNo(data, res);
+  } else {
+
+    var requisitionfiles = fs.readdirSync(newPath).map(fileName => {
+      return fileName;
+    }).filter(isRequisition);
+
+    var nextNumber = getNextRequisitionNo(requisitionfiles);
+    var newFileName = path.join(newPath, `${data.masterAccessionNo}.REQ${nextNumber}.pdf`);
+    
+    fs.rename(data.existingPath, newFileName, function (err) {
+      if (err) throw err
+      data.url = `/scanfiles/${req.params.folderName}`;
+      data.displayName = req.params.folderName;
+      data.filePath = newPath;
+      res.render('scanfile_moved', data);
+    })
+  }
+});
+
+function getNextRequisitionNo (files) {
+  var existingNumbers = files.map(function (file) {
+    var regx = /\.REQ(\d+)\./
+    var match = file.match(regx);
+    return match[1];
+  }).sort(function (a, b) {
+    return a - b;
+  });
+
+  var largestNumber = Math.max.apply(null, existingNumbers);
+  return largestNumber + 1;
+}
+
+function renderInvalidMasterAccessionNo (data, res) {
+  res.render('invalid_master_accession', {
+    masterAccessionNo: data.masterAccessionNo,
+    backUrl: `/scanfiles/${data.folderName}/${data.fileName}`,
+  });
+}
+
+app.post('/scanfiles/:folderName/:fileName/back', urlencodedParser, (req, res) => {
+  var data = {
+    folderName: req.params.folderName,
+    fileName: req.params.fileName    
+  }
+  renderScanFiles(data, res);
+});
 
 function getCasePath(masterAccessionNo) {
   var dashSplit = masterAccessionNo.split('-');
   var year = `20${dashSplit[0]}`;
   var number = Number(dashSplit[1]);
 
-  var thousandNo = (Math.ceil(number / 1000) * 1000) - 1000;
-  var filePath = path.join(rootFolder, year, `${thousandNo.toString().padStart(5, 0)}-${(thousandNo + 999).toString().padStart(5, 0)}`)
+  var filePath = path.join(process.env.ACCESSION_DOCUMENTS_LOCATION, year, getBaseFolder(number), masterAccessionNo);
 
-  return `${filePath}/${masterAccessionNo}.pdf`;
+  console.log(`case document path: ${filePath}`)
+  return filePath;
 }
 
+function isMasterAccessionValid(masterAccessionNo) {
+  return masterAccessionNo.test(/^\d\d-\d+/g);
+}
 
-app.listen(port);
+var getBaseFolder = (number) => {
+  var lowerBound = 0;
+  var upperBound = 0;
+
+  for(var i=0; i<=number; i+=1000) {
+    lowerBound = i;
+    upperBound = i + 999;
+  }
+
+  if(lowerBound == 0) lowerBound = 1;
+  return `${lowerBound.toString().padStart(5, 0)}-${upperBound.toString().padStart(5, 0)}`;
+}
+
+app.listen(process.env.APP_PORT, function (err) {
+  if(err) return console.log(err);
+  console.log(`Express listening on port: ${process.env.APP_PORT}`);
+});
